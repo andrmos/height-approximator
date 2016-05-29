60626e918bdcf0a71a72190d2ca8d7d58517bd52
andrmos <mossige.a@gmail.com>
2016-05-29 23:47:24 +0200

    Add presenter interface

---
+++ app/src/main/java/com/mossige/andre/heightapproximator/interfaces/Presenter.java
@@ -0,0 +1,17 @@
+package com.mossige.andre.heightapproximator.interfaces;
+
+/**
+ * Presenter interface responsible for handling the communication between the views and the models.
+ *
+ * Created by André on 29.05.2016.
+ */
+public interface Presenter {
+
+    /**
+     * Called when the button that interacts with the approximator is clicked.
+     */
+    void onButtonClicked();
+
+    // TODO: 29.05.2016 Add activity lifecycle methods.
+
+}
