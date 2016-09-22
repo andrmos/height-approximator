b8ddd41b7afeea439c2a8ba1a7b3a1a849f1389b
andrmos <mossige.a@gmail.com>
2016-09-22 20:30:32 +0200

    Implement simple main activity interface

--- app/src/main/java/com/mossige/andre/heightapproximator/MainActivity.java
+++ app/src/main/java/com/mossige/andre/heightapproximator/MainActivity.java
@@ -2,7 +2,6 @@ package com.mossige.andre.heightapproximator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -14,30 +13,36 @@ import com.mossige.andre.heightapproximator.interfaces.Presenter;
 public class MainActivity extends AppCompatActivity implements MainView {
 
     private static final String TAG = "MainActivity";
-    private Button button;
+    private static final int UPDATE_INTERVAL_MILLIS = 50;
+
     private TextView heightText;
+    private Presenter presenter;
+    private Thread updateThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Presenter presenter = new PresenterImpl(this);
+        presenter = new PresenterImpl(this);
 
-        button = (Button) findViewById(R.id.button1);
+        Button button = (Button) findViewById(R.id.button1);
         button.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch(event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         // Pressed
-                        Log.i(TAG, "onTouch: pressed");
                         presenter.onButtonPressed();
+                        updateThread.start();
+
                         return true;
                     case MotionEvent.ACTION_UP:
                         // Released
-                        Log.i(TAG, "onTouch: released");
                         presenter.onButtonReleased();
+                        updateThread.interrupt();
+
+                        createUpdateThread();
                         return true;
                 }
                 return false;
@@ -46,10 +51,34 @@ public class MainActivity extends AppCompatActivity implements MainView {
 
         heightText = (TextView) findViewById(R.id.heightText);
 
+        createUpdateThread();
     }
 
     @Override
     public void updateTextView(String text) {
         heightText.setText(text);
     }
+
+    private void createUpdateThread() {
+        updateThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(UPDATE_INTERVAL_MILLIS);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateTextView("" + presenter.getHeight());
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                    // Stopped
+                }
+            }
+        };
+    }
+
 }
